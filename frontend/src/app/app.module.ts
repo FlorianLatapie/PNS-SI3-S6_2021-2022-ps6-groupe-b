991898c46bb560faf3a917076391d85ec97b948e
MariusLesaulnier <marius.lesaulnier@etu.unice.fr>
2022-03-24 13:50:37 +0100

    ajout de la navigation entre question

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -24,6 +24,7 @@ import { AboutPageComponent } from './pages/about-page/about-page.component';
 import { AccountPageComponent } from './pages/account-page/account-page.component';
 import { QuizListSelectComponent } from './quizzes/quiz-list-select/quiz-list-select.component';
 import { PlayQuizPageComponent } from './pages/play-quiz-page/play-quiz-page.component';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 
 @NgModule({
   declarations: [
@@ -53,6 +54,7 @@ import { PlayQuizPageComponent } from './pages/play-quiz-page/play-quiz-page.com
     ReactiveFormsModule,
     AppRoutingModule,
     HttpClientModule,
+    BrowserAnimationsModule,
   ],
   providers: [],
   bootstrap: [AppComponent]
--- frontend/src/app/pages/play-quiz-page/play-quiz-page.component.ts
+++ frontend/src/app/pages/play-quiz-page/play-quiz-page.component.ts
@@ -1,5 +1,7 @@
 import { Component, Input, OnInit } from '@angular/core';
 import { Quiz } from 'src/models/quiz.model';
+import {MatButtonModule} from '@angular/material/button';
+
 
 @Component({
   selector: 'app-play-quiz-page',
@@ -10,11 +12,25 @@ export class PlayQuizPageComponent implements OnInit {
 
   @Input()
   quiz : Quiz;
+  currentQuestion = 0;
+  answerSelected = false;
 
   constructor() { }
 
 
   ngOnInit(): void {
+    
+  }
+
+  onAnswer(option: boolean){
+    if(this.quiz.questions.length>this.currentQuestion){
+    this.answerSelected = true;
+    setTimeout(()=>{
+      this.currentQuestion++;
+      this.answerSelected = false;
+    }, 1000);
+    }
+    
     
   }
 
