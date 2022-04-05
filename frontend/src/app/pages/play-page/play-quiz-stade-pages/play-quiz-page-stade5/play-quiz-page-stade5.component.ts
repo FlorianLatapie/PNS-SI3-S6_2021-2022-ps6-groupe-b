d6fe7209fa05e1208362c0a3e66567a58edf47d3
Florian LATAPIE <florian.latapie@etu.unice.fr>
2022-04-05 11:15:45 +0200

    amélioration du style

--- frontend/src/app/pages/play-page/play-quiz-stade-pages/play-quiz-page-stade5/play-quiz-page-stade5.component.ts
+++ frontend/src/app/pages/play-page/play-quiz-stade-pages/play-quiz-page-stade5/play-quiz-page-stade5.component.ts
@@ -1,9 +1,8 @@
-import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute, Router } from '@angular/router';
-import { Question } from 'src/models/question.model';
-import { Quiz } from 'src/models/quiz.model';
-import { QuizService } from 'src/services/quiz.service';
-
+import {Component, OnInit} from '@angular/core';
+import {ActivatedRoute, Router} from '@angular/router';
+import {Question} from 'src/models/question.model';
+import {Quiz} from 'src/models/quiz.model';
+import {QuizService} from 'src/services/quiz.service';
 
 
 @Component({
@@ -22,16 +21,16 @@ export class PlayQuizPageStade5Component implements OnInit {
   constructor(private route: ActivatedRoute, private quizService: QuizService, private router: Router) {
     this.quizService.quizSelected$.subscribe((quiz) => {
       this.quiz = quiz;
-      this.quiz.correctQuestions=0;
-      this.quiz.incorrectQuestions=0;
+      this.quiz.correctQuestions = 0;
+      this.quiz.incorrectQuestions = 0;
       this.questions = [...this.quiz.questions];
-      
-      for(var q of this.questions){
-        q.correctAnswers=0;
-        q.incorrectAnswers=0;
+
+      for (var q of this.questions) {
+        q.correctAnswers = 0;
+        q.incorrectAnswers = 0;
         q.currentImage = 0;
       }
-      
+
       // mélange les questions
       this.shuffleArray(this.questions);
       this.currentQuestion = this.questions[0];
@@ -45,16 +44,14 @@ export class PlayQuizPageStade5Component implements OnInit {
     const id = this.route.snapshot.paramMap.get('id');
     this.quizService.setSelectedQuiz(id);
   }
-  
-  
-  
-  onAnswer(option: boolean){
-    if(option){
+
+
+  onAnswer(option: boolean) {
+    if (option) {
       this.quiz.correctQuestions++;
       this.currentQuestion.correctAnswers++;
-      
-    } 
-    else{
+
+    } else {
       this.quiz.incorrectQuestions++;
       this.currentQuestion.incorrectAnswers++;
     }
@@ -62,63 +59,60 @@ export class PlayQuizPageStade5Component implements OnInit {
   }
 
 
-  nextQuestion(){
-      setTimeout(()=>{
-        if(this.reAddQuestionIntoQuiz(this.currentQuestion))
-        {
-          this.currentQuestion.currentImage = (this.currentQuestion.currentImage + 1)%3; //inutile si 3 images
-        }
-        else if(this.questions.length<=0)
-        {
-            this.endOfQuiz=true;
-            return;
-        }        
-        this.initNextQuestion();
-      }, 1000);
-    }
+  nextQuestion() {
+    setTimeout(() => {
+      if (this.reAddQuestionIntoQuiz(this.currentQuestion)) {
+        this.currentQuestion.currentImage = (this.currentQuestion.currentImage + 1) % 3; //inutile si 3 images
+      } else if (this.questions.length <= 0) {
+        this.endOfQuiz = true;
+        return;
+      }
+      this.initNextQuestion();
+    }, 1000);
+  }
 
 
-  reAddQuestionIntoQuiz(question : Question): boolean{
-    if(question.imageUrls.length> this.currentQuestion.currentImage){
-      if((question.incorrectAnswers<3 && question.correctAnswers<1)){ //si trop de mauvaises réponses : oublit, si au moins deux bonnes réponses : on sait
-        console.log("readdquestionIntoQuiz: "+this.currentQuestion.id);
-        console.log("bonne réponses : "+this.currentQuestion.correctAnswers);
-        console.log("mauvaise réponses : "+this.currentQuestion.incorrectAnswers);
-        console.log("");
+  reAddQuestionIntoQuiz(question: Question): boolean {
+    if (question.imageUrls.length > this.currentQuestion.currentImage) {
+      if ((question.incorrectAnswers < 3 && question.correctAnswers < 1)) {
+        // si trop de mauvaises réponses : oublie, si au moins deux bonnes réponses : on sait
+        console.log('readdquestionIntoQuiz: ' + this.currentQuestion.id);
+        console.log('bonnes réponses : ' + this.currentQuestion.correctAnswers);
+        console.log('mauvaise réponses : ' + this.currentQuestion.incorrectAnswers);
+        console.log('');
         return true;
       }
-      this.questions.splice(this.questions.indexOf(question),1);
-      console.log("\nremoving :"+ this.currentQuestion.id);
-      console.log("question.incorrectAnswers : "+ question.incorrectAnswers +" question.correctAnswers : "+question.correctAnswers);
+      this.questions.splice(this.questions.indexOf(question), 1);
+      console.log('\nremoving :' + this.currentQuestion.id);
+      console.log('question.incorrectAnswers : ' + question.incorrectAnswers + ' question.correctAnswers : ' + question.correctAnswers);
       return false;
     }
     return false;
   }
 
 
-
-  initNextQuestion(){
+  initNextQuestion() {
     this.shuffleArray(this.questions);
     this.currentQuestion = this.questions[0];
-     //mélange les réponses
+    // mélange les réponses
     this.shuffleArray(this.currentQuestion.answers);
   }
-  
+
   shuffleArray(array) {
     for (var i = array.length - 1; i > 0; i--) {
-        var j = Math.floor(Math.random() * (i + 1));
-        var temp = array[i];
-        array[i] = array[j];
-        array[j] = temp;
+      var j = Math.floor(Math.random() * (i + 1));
+      var temp = array[i];
+      array[i] = array[j];
+      array[j] = temp;
     }
-}
+  }
 
-reloadQuiz(){
-  let currentUrl = this.router.url;
+  reloadQuiz() {
+    let currentUrl = this.router.url;
     this.router.routeReuseStrategy.shouldReuseRoute = () => false;
     this.router.onSameUrlNavigation = 'reload';
     this.router.navigate([currentUrl]);
-}
+  }
 
 
 }
