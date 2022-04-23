8211ec64f4ffbd4c7e5f6caf007f699df81b8b38
LoicLeBris <loic.le-bris@etu.univ-cotedazur.fr>
2022-04-23 19:45:30 +0200

    Modification nom prenom user

--- frontend/src/app/pages/account-page/account-page.component.ts
+++ frontend/src/app/pages/account-page/account-page.component.ts
@@ -1,6 +1,7 @@
-import {Component, OnInit} from '@angular/core';
+import {Component, Input, OnInit} from '@angular/core';
 import {User} from '../../../models/user.model';
 import {UserService} from '../../../services/user.service';
+import {Router} from '@angular/router';
 
 @Component({
   selector: 'app-account-page',
@@ -8,9 +9,11 @@ import {UserService} from '../../../services/user.service';
   styleUrls: ['./account-page.component.scss']
 })
 export class AccountPageComponent implements OnInit {
+  private previousUrl: string;
   public userSelected: User;
 
-  constructor(private userService: UserService) {
+  constructor(private router: Router, private userService: UserService) {
+    this.previousUrl = '/' + this.router.getCurrentNavigation().previousNavigation.finalUrl.root.children.primary.segments[0].path;
     userService.userSelected$.subscribe(event => {
       this.userSelected = event;
       console.log('il y a un utilisateur :', this.userSelected);
@@ -40,5 +43,37 @@ export class AccountPageComponent implements OnInit {
       this.userSelected.stade = stade + '';
       this.userService.updateUser(this.userSelected);
     }
+  }
+
+  modifyName(str: string): void {
+    const text = document.getElementById(str);
+    const input = document.createElement('input');
+    if (str === 'first_name'){
+      input.placeholder = text.firstChild.textContent;
+    } else {
+      input.placeholder = text.firstChild.textContent;
+    }
+    text.firstChild.replaceWith(input);
+    const btn = document.getElementById(str + '_button');
+    btn.firstChild.replaceWith('Valider');
+    btn.removeEventListener('click', () => this.modifyName(str));
+    btn.addEventListener('click', () => this.checkName(str, input.value));
+  }
+
+  private checkName(str: string, newName: string): void {
+    if (str === 'first_name'){
+      this.userSelected.firstName = newName;
+      this.userService.updateUser(this.userSelected);
+      document.getElementById(str).firstChild.replaceWith(this.userSelected.firstName);
+    } else {
+      this.userSelected.lastName = newName;
+      this.userService.updateUser(this.userSelected);
+      document.getElementById(str).firstChild.replaceWith(this.userSelected.lastName);
+    }
+    document.getElementById('name_header').firstChild.replaceWith(this.userSelected.firstName + ' ' + this.userSelected.lastName)
+    const btn = document.getElementById(str + '_button');
+    btn.firstChild.replaceWith('Modifier');
+    btn.removeEventListener('click', () => this.checkName(str, newName));
+    btn.addEventListener('click', () => this.modifyName(str));
   }
 }
